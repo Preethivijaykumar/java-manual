cf83d379bdbd466ef531f5523a290260e44e1ffd
Preethivijaykumar <41036441+Preethivijaykumar@users.noreply.github.com>
2018-08-21 16:10:44 +0530

    Add files via upload

---
+++ Programmer.java
@@ -0,0 +1,11 @@
+package small;
+
+public class Programmer extends First{
+	  int bonus=240000;  
+	 public static void main(String args[]){  
+   Programmer p=new Programmer();  
+	   System.out.println("Programmer salary is:"+p.salary);  
+	   System.out.println("Bonus of Programmer is:"+p.bonus);  
+	}  
+	}  
+
