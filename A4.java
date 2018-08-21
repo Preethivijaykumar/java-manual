799aa45ae9038b7096eabf25d2c40c62cfd37f24
Preethivijaykumar <41036441+Preethivijaykumar@users.noreply.github.com>
2018-08-21 16:12:18 +0530

    Add files via upload

---
+++ A4.java
@@ -0,0 +1,13 @@
+package first;
+
+
+public class A4{  
+	  int data=1000;  
+      A4(){  
+	   B b=new B(this);  
+	   b.display();  
+	  }  
+	  public static void main(String args[]){  
+	   A4 a=new A4();  
+	  }  
+	}  
---
+++ B.java
@@ -0,0 +1,13 @@
+package first;
+
+public class B {
+    A4 obj;  
+	  B(A4 obj){  
+	    this.obj=obj;  
+	  }  
+	  void display(){  
+	    System.out.println(obj.data);//using data member of A4 class  
+	  }  
+	}  
+
+
