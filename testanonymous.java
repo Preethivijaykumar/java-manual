7d1bd437357a15c918f6a5d365c473dafba475e1
Preethivijaykumar <41036441+Preethivijaykumar@users.noreply.github.com>
2018-08-28 20:49:23 +0530

    Add files via upload

---
+++ testanonymous.java
@@ -0,0 +1,13 @@
+package manual;
+
+public class testanonymous {
+  
+      public static void main(String args[]){  
+	  person p=new person(){  
+	  void eat(){System.out.println("nice fruits");}  
+	  };  
+	  p.eat();  
+	 }  
+	}
+
+
